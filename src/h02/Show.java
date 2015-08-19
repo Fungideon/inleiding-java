332571f5027e9db6cf4f87733c2e4ac8c91d0100
Timo Moederzoon <tigimoti@gmail.com>
2015-08-19 14:42:07 +0200

    fixed some derps

--- src/h02/Show.java
+++ src/h02/Show.java
@@ -1,16 +1,5 @@
-/*
+package h02;
 
-@(#)Show.java 1.0 03/02/26
-
-*
-
-Voorbeeld 2.1
-
-*
-
-*/
-
-//importeer de benodigde klassen uit de Java bibliotheek
 import java.awt.*;
 import java.applet.*;
 
