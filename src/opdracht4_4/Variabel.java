bf98878aeb14ef7d8bf96e2a94a49886df26d159
Timo Moederzoon <tigimoti@gmail.com>
2015-09-01 15:06:07 +0200

    + Variabelen Staafdiagram (staat onder opdracht4_4 Variabel.java)

--- src/opdracht4_4/Variabel.java
+++ src/opdracht4_4/Variabel.java
@@ -3,11 +3,17 @@ package opdracht4_4;
 import java.awt.*;
 import java.applet.*;
 
-public class Gewicht extends Applet {
+public class Variabel extends Applet {
+	int valerie, hans, jeroen, valerie2, hans2, jeroen2;
 	
 	public void init(){
-		setSize(250,250);
-		
+		hans = 80;
+		valerie = 40;
+		jeroen =  100;
+		hans2 = 200 - hans;
+		valerie2 = 200 - valerie;
+		jeroen2 = 200 - jeroen;
+		setSize(250,250);	
 	}
 	
 	public void paint (Graphics g){
@@ -17,12 +23,15 @@ public class Gewicht extends Applet {
 		g.drawString("Hans", 80, 220);
 		g.drawString("Jeroen", 140, 220);
 		//gewicht bars
+		//Valerie
 		g.setColor(Color.YELLOW);
-		g.fillRect(25, 160, 25, 40);
+		g.fillRect(25, valerie2, 25, valerie);
+		//Hans
 		g.setColor(Color.BLUE);
-		g.fillRect(80, 120, 25, 80);
+		g.fillRect(80, hans2, 25, hans);
+		//Jeroen
 		g.setColor(Color.RED);
-		g.fillRect(140, 100, 25, 100);
+		g.fillRect(140, jeroen2, 25, jeroen);
 		//lijn verdeling en text
 		g.setColor(Color.black);
 		g.drawLine(25, 180, 250, 180);
@@ -37,7 +46,8 @@ public class Gewicht extends Applet {
 		g.drawString("100", 1, 100);
 		g.drawLine(25, 80, 250, 80);
 		g.drawString("120", 1, 80);
-
+		g.drawLine(25, 60, 250, 60);
+		g.drawString("140", 1, 60);
 
 		
 	}
